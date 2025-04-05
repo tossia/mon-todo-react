1f42436850d4323b44c38c94dc4f88985bebef1f
tossia <tossia2000@yahoo.fr>
2025-04-05 21:51:59 +0200

    add & delete task OK

--- src/App.jsx
+++ src/App.jsx
@@ -53,14 +53,17 @@ function App(props) {
    * @param {string} id - The id of the task to toggle.
    * @returns {undefined}
    */
-  function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map((task) => {
-      if (id === task.id) {
-        return { ...task, completed: !task.completed };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
+  const toggleTaskCompleted = (id) => {
+    const task = tasks.find((task) => task.id === id);
+    const updatedTasks = { ...task, completed: !task.completed };
+
+    axios
+      .put(`http://localhost:5000/tasks/${id}`, updatedTasks)
+      .then(() => setTasks(tasks.map((task) => task.id === id ? updatedTasks : task))
+      )
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   /**
@@ -70,19 +73,38 @@ function App(props) {
    * @returns {undefined}
    */
 
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
-    setTasks(remainingTasks);
-  }
+
+  /**
+   * Deletes a task with the given `id` from the task list.
+   *
+   * @param {string} id - The id of the task to delete.
+   * @returns {undefined}
+   */
+  const deleteTask = (id) => {
+    console.log(tasks)
+    axios
+      .delete(`http://localhost:5000/tasks/${id}`)
+      .then((response) => {
+        if (response.status === 200) {
+          setTasks(tasks.filter((task) => task.id !== id));
+        } else {
+          console.error(`Error deleting task with ID ${id}`);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   const taskList = Array.isArray(tasks) ? tasks
     ?.filter(FILTER_MAP[filter] || (() => true))
     .map((task) => (
       <Todo
+        key={task.id}
+        task={task}
         id={task.id}
         name={task.name}
         completed={task.completed}
-        key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
         editTask={editTask}
@@ -104,13 +126,13 @@ function App(props) {
    * @param {string} name - The name of the task to add.
    * @returns {undefined}
    */
-  function addTask(name) {
+  const addTask = (name) => {
     if (name != "") {
       const newTask = { name, completed: false };
 
       axios
         .post('http://localhost:5000/tasks', newTask)
-        .then((res) => setTasks(...tasks, res.data))
+        .then((res) => setTasks([...tasks, res.data]))
         .catch((err) => {
           console.log(err)
         })
